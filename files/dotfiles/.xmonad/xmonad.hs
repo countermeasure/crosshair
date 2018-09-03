224a13b200571bdfefff68d83e8f8654f0791115
Sky <sky@skychristensen.com>
2018-09-03 12:20:40 -0700

    Install urxvt

--- files/dotfiles/.xmonad/xmonad.hs
+++ files/dotfiles/.xmonad/xmonad.hs
@@ -1,7 +1,18 @@
 import XMonad
 import XMonad.Hooks.ManageDocks
+import XMonad.Util.EZConfig
+
+
+myTerminal = "x-terminal-emulator"
+
 
 main = do
     spawn "xmobar"
 
-    xmonad $ docks def { layoutHook = avoidStruts (Full)}
+    xmonad $ docks def
+        { layoutHook = avoidStruts (Full)
+        , terminal = myTerminal
+        }
+
+        `additionalKeys`
+        [ ((mod1Mask, xK_Return), spawn myTerminal) ]
