f395d4f0e47cb7b17ca7e9f652879293218498c7
Sky <sky@skychristensen.com>
2018-10-12 00:18:53 -0400

    Install KeePassX

--- files/dotfiles/.xmonad/xmonad.hs
+++ files/dotfiles/.xmonad/xmonad.hs
@@ -18,6 +18,7 @@ main = do
         [ ((mod1Mask, xK_Return), spawn myTerminal)
         , ((mod1Mask, xK_b), spawn "x-www-browser")
         , ((mod1Mask .|. shiftMask, xK_b), spawn "chromium")
+        , ((mod1Mask, xK_p), spawn "keepassx")
         , ((mod1Mask, xK_r), spawn "rofi -show run")
         , ((mod1Mask, xK_t), spawn "torbrowser-launcher")
         ]
