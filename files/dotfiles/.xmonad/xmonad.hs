56969aa0066921731184f66b311eb7be6f53323c
Sky <sky@skychristensen.com>
2018-10-11 23:39:03 -0400

    Install suckless-tools to get dmenu

--- files/dotfiles/.xmonad/xmonad.hs
+++ files/dotfiles/.xmonad/xmonad.hs
@@ -18,5 +18,6 @@ main = do
         [ ((mod1Mask, xK_Return), spawn myTerminal)
         , ((mod1Mask, xK_b), spawn "x-www-browser")
         , ((mod1Mask .|. shiftMask, xK_b), spawn "chromium")
+        , ((mod1Mask, xK_r), spawn "rofi -show run")
         , ((mod1Mask, xK_t), spawn "torbrowser-launcher")
         ]
